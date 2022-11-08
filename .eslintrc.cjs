c6567f41d2d448082bbc96dab2626aa1d7d1d526
Kryštof Rainisch <stofi@users.noreply.github.com>
2022-11-08 10:18:25 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
