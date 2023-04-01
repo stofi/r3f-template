72319e9dafc34d62405ba9ae2a7b3d3d215ac556
Krystof Rainisch <krystof.rainisch@hey.com>
2023-04-01 12:24:05 +0200

    Add render target and custom effect

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -8,6 +8,7 @@ import { Leva } from 'leva'
 import { Perf } from 'r3f-perf'
 
 import Scene from '#/Scene'
+import Effects from '$/Effects'
 
 export default function Experience(props: { enableDebug?: boolean }) {
   return (
@@ -32,6 +33,7 @@ export default function Experience(props: { enableDebug?: boolean }) {
         <Suspense>
           <Physics>
             {props.enableDebug && <Debug />}
+            <Effects />
             <Scene />
           </Physics>
         </Suspense>
